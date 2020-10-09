0f579009f50877a79ff044c441d809bb74239459
stdavis <stdavis@utah.gov>
2020-10-09 09:47:31 -0600

    feat: add run and deploy scripts

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+read -p 'Project: ' PROJECT_ID
+gcloud config set project $PROJECT_ID
+echo "building image..."
+gcloud builds submit --tag gcr.io/$PROJECT_ID/masquerade
+echo "deploying image..."
+gcloud run deploy masquerade --image gcr.io/$PROJECT_ID/masquerade --platform managed --region us-central1
--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 export FLASK_APP=src/masquerade/main.py && export FLASK_ENV=development && flask run --cert=cert.pem --key key.pem
