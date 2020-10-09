cc91a43184915a254643ddf7ca422cbc0222c4e8
stdavis <stdavis@utah.gov>
2020-10-09 16:40:02 -0600

    chore: add port number to deploy command

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ gcloud config set project $PROJECT_ID
 echo "building image..."
 gcloud builds submit --tag gcr.io/$PROJECT_ID/masquerade
 echo "deploying image..."
-gcloud run deploy masquerade --image gcr.io/$PROJECT_ID/masquerade --platform managed --region us-central1
+gcloud run deploy masquerade --image gcr.io/$PROJECT_ID/masquerade --platform managed --region us-central1 --port 8000
