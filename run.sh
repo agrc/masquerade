ff614fb6127b6862c9d65a129c61a0dfa2e9dd0b
stdavis <stdavis@utah.gov>
2020-12-07 17:46:49 -0700

    feat: make dev server able to be hit by another machine
    Also, include docs to support hitting the dev server from a windows VM.

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 export FLASK_APP=src/masquerade/main.py
 export FLASK_ENV=development
 export WEB_API_KEY=$(<.apikey)
-flask run --cert=cert.pem --key key.pem
+flask run --cert=cert.pem --key key.pem --host=0.0.0.0
