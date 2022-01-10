de9e8e4f85b8eac03b3ac1489963cdef841c7871
stdavis <stdavis@utah.gov>
2022-01-10 10:59:48 -0700

    fix: add input validation to table constructor

--- tests/open_sgid/test_AddressPointTable.py
+++ tests/open_sgid/test_AddressPointTable.py
@@ -6,7 +6,7 @@ A module that contains tests for the open_sgid.AddressPointTable class
 
 from masquerade.providers.open_sgid import ADDSYSTEM, CITY, FULLADD, POINT, AddressPointTable
 
-table = AddressPointTable('table_name', FULLADD, [ADDSYSTEM, CITY], POINT)
+table = AddressPointTable('table_name', FULLADD, POINT, additional_out_fields=[ADDSYSTEM, CITY])
 
 
 def test_get_suggestion_from_record_with_city():
