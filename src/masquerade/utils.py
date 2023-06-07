ad36cf6c52b2e07761ee23e668b30d5263452e17
steveoh <sgourley@utah.gov>
2023-06-07 15:18:19 -0600

    fix: add defensive code to method
    this method isn't always called with strings

--- src/masquerade/utils.py
+++ src/masquerade/utils.py
@@ -8,4 +8,10 @@ Utility functions
 def cleanse_text(text):
     """ removes leading or trailing spaces and quotes
     """
+    if text is None:
+        return None
+
+    if not isinstance(text, str):
+        return text
+
     return text.strip().replace('"', '').replace('\'', '')
