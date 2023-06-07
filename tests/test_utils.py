a3f383bc775f060d5d7830043945182ff9f81690
steveoh <sgourley@utah.gov>
2023-06-07 15:18:35 -0600

    test: add a new test for cleanse_text

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,3 +8,9 @@ def test_removes_spaces():
 def test_removes_quotes():
     assert cleanse_text('"hello"') == 'hello'
     assert cleanse_text('test \'hello') == 'test hello'
+
+
+def test_cleanse_text_without_strings():
+    assert cleanse_text(None) == None
+    assert cleanse_text(1) == 1
+    assert cleanse_text({}) == {}
