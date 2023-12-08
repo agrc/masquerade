5bf0b561aa030a9533074ce7930ba230ad1db792
stdavis <stdavis@utah.gov>
2023-12-08 11:53:58 -0700

    fix: correctly get output spatial reference for POST requests
    Includes a refactor to move the helper function out into the utils module.

--- src/masquerade/utils.py
+++ src/masquerade/utils.py
@@ -3,6 +3,11 @@
 """
 Utility functions
 """
+import json
+
+WGS84 = 4326
+WEB_MERCATOR = 3857
+OLD_WEB_MERCATOR = 102100
 
 
 def cleanse_text(text):
@@ -14,3 +19,28 @@ def cleanse_text(text):
         return text
 
     return text.strip().replace('"', "").replace("'", "")
+
+
+def get_out_spatial_reference(incoming_request):
+    """get the desired output spatial reference from the request"""
+    out_sr_param_name = "outSR"
+
+    if incoming_request.method == "GET":
+        request_params = incoming_request.args
+    else:
+        request_params = incoming_request.form
+
+    if out_sr_param_name in request_params:
+        out_sr_param = request_params.get(out_sr_param_name)
+        try:
+            request_wkid = int(out_sr_param)
+        except ValueError:
+            request_wkid = json.loads(out_sr_param)["wkid"]
+    else:
+        request_wkid = WGS84
+
+    #: switch out old mercator for new one otherwise, pass it through
+    return (
+        request_wkid,
+        WEB_MERCATOR if request_wkid == OLD_WEB_MERCATOR else request_wkid,
+    )
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,6 @@
-from masquerade.utils import cleanse_text
+from unittest.mock import MagicMock
+
+from masquerade.utils import cleanse_text, get_out_spatial_reference
 
 
 def test_removes_spaces():
@@ -14,3 +16,35 @@ def test_cleanse_text_without_strings():
     assert cleanse_text(None) is None
     assert cleanse_text(1) == 1
     assert cleanse_text({}) == {}
+
+
+def test_get_out_spatial_reference_get_request():
+    request = MagicMock()
+    request.method = "GET"
+    request.args = {"outSR": 3857}
+
+    assert get_out_spatial_reference(request) == (3857, 3857)
+
+
+def test_get_out_spatial_reference_get_request_json():
+    request = MagicMock()
+    request.method = "GET"
+    request.args = {"outSR": '{"wkid": 3857}'}
+
+    assert get_out_spatial_reference(request) == (3857, 3857)
+
+
+def test_get_out_spatial_reference_default():
+    request = MagicMock()
+    request.method = "GET"
+    request.args = {}
+
+    assert get_out_spatial_reference(request) == (4326, 4326)
+
+
+def test_get_out_spatial_reference_post_request():
+    request = MagicMock()
+    request.method = "POST"
+    request.form = {"outSR": 3857}
+
+    assert get_out_spatial_reference(request) == (3857, 3857)
